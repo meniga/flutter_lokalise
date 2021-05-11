992f0a513798fce5b6dd0f1273f2d168a44bf576
Guy Kogus <guy.kogus@ridewithvia.com>
2021-05-11 19:36:24 +0200

    Fix nullability

--- lib/src/client/files_download_request_body.dart
+++ lib/src/client/files_download_request_body.dart
@@ -10,19 +10,19 @@ class FilesDownloadRequestBody with _$FilesDownloadRequestBody {
     @JsonKey(name: "original_filenames")
     @Default(false)
         bool? originalFilenames,
-    @JsonKey(name: "all_platforms") @Default(true) bool? allPlatforms,
-    @JsonKey(name: "plural_format") @Default("icu") String? pluralFormat,
+    @JsonKey(name: "all_platforms") @Default(true) bool allPlatforms,
+    @JsonKey(name: "plural_format") @Default("icu") String pluralFormat,
     @JsonKey(name: "placeholder_format")
     @Default("icu")
-        String? placeholderFormat,
+        String placeholderFormat,
     @JsonKey(name: "include_tags") Iterable<String>? includeTags,
-    @JsonKey(name: "include_comments") @Default(true) bool? includeComments,
+    @JsonKey(name: "include_comments") @Default(true) bool includeComments,
     @JsonKey(name: "include_description")
     @Default(true)
-        bool? includeDescription,
+        bool includeDescription,
     @JsonKey(name: "json_unescaped_slashes")
     @Default(true)
-        bool? jsonUnescapedSlashes,
+        bool jsonUnescapedSlashes,
   }) = _FilesDownloadRequestBody;
 
   factory FilesDownloadRequestBody.fromJson(Map<String, dynamic> json) =>
